ade9fbbf34f87912015eb329e8107f06bf2b9cfc
Gustavo Coelho Reis <gustavogcrcoelhoreis2005@gmail.com>
2025-05-12 14:57:21 -0300

    código finalizado

--- controle.py
+++ controle.py
@@ -12,8 +12,8 @@ st.title("📊 Dashboard Financeiro Pessoal")
 
 
 client = OpenAI(
-   # api_key=st.secrets["GROQ_API_KEY"],
-    api_key=("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"),
+    api_key=st.secrets["GROQ_API_KEY"],
+    #api_key=("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"),
     base_url="https://api.groq.com/openai/v1"
 )
 
